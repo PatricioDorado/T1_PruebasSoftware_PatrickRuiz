926b1b88119f3c980c99f03ec5ebe6b4e53a7274
Patricio Dorado <elpatopato22@gmail.com>
2026-03-31 12:10:06 -0500

    Avances

---
+++ src/main/java/edu/pe/cibertec/infracciones/service/impl/InfractorServiceImpl.java
@@ -0,0 +1,101 @@
+package edu.pe.cibertec.infracciones.service.impl;
+
+import edu.pe.cibertec.infracciones.dto.InfractorRequestDTO;
+import edu.pe.cibertec.infracciones.dto.InfractorResponseDTO;
+import edu.pe.cibertec.infracciones.exception.InfractorNotFoundException;
+import edu.pe.cibertec.infracciones.exception.VehiculoNotFoundException;
+import edu.pe.cibertec.infracciones.model.EstadoMulta;
+import edu.pe.cibertec.infracciones.model.Infractor;
+import edu.pe.cibertec.infracciones.model.Multa;
+import edu.pe.cibertec.infracciones.model.Vehiculo;
+import edu.pe.cibertec.infracciones.repository.InfractorRepository;
+import edu.pe.cibertec.infracciones.repository.MultaRepository;
+import edu.pe.cibertec.infracciones.repository.VehiculoRepository;
+import edu.pe.cibertec.infracciones.service.IInfractorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class InfractorServiceImpl implements IInfractorService {
+
+    private final InfractorRepository infractorRepository;
+    private final VehiculoRepository vehiculoRepository;
+    private final MultaRepository multaRepository;
+
+    @Override
+    public InfractorResponseDTO registrarInfractor(InfractorRequestDTO dto) {
+        Infractor infractor = new Infractor();
+        infractor.setDni(dto.getDni());
+        infractor.setNombre(dto.getNombre());
+        infractor.setApellido(dto.getApellido());
+        infractor.setEmail(dto.getEmail());
+        infractor.setBloqueado(false);
+        return mapToResponse(infractorRepository.save(infractor));
+    }
+
+    @Override
+    public InfractorResponseDTO obtenerInfractorPorId(Long id) {
+        Infractor infractor = infractorRepository.findById(id)
+                .orElseThrow(() -> new InfractorNotFoundException(id));
+        return mapToResponse(infractor);
+    }
+
+    @Override
+    public List<InfractorResponseDTO> obtenerTodos() {
+        return infractorRepository.findAll()
+                .stream()
+                .map(this::mapToResponse)
+                .toList();
+    }
+
+    @Override
+    public void asignarVehiculo(Long infractorId, Long vehiculoId) {
+        Infractor infractor = infractorRepository.findById(infractorId)
+                .orElseThrow(() -> new InfractorNotFoundException(infractorId));
+        Vehiculo vehiculo = vehiculoRepository.findById(vehiculoId)
+                .orElseThrow(() -> new VehiculoNotFoundException(vehiculoId));
+        infractor.getVehiculos().add(vehiculo);
+        infractorRepository.save(infractor);
+    }
+
+
+    private InfractorResponseDTO mapToResponse(Infractor infractor) {
+        InfractorResponseDTO dto = new InfractorResponseDTO();
+        dto.setId(infractor.getId());
+        dto.setDni(infractor.getDni());
+        dto.setNombre(infractor.getNombre());
+        dto.setApellido(infractor.getApellido());
+        dto.setEmail(infractor.getEmail());
+        dto.setBloqueado(infractor.isBloqueado());
+        return dto;
+    }
+
+    @Override
+    public Double calcularDeuda(Long infractorId) {
+        Infractor infractor = infractorRepository.findById(infractorId).orElseThrow(() -> new InfractorNotFoundException(infractorId));
+        List<Multa> pendientes = multaRepository.findByInfractor_IdAndEstado(infractorId, EstadoMulta.PENDIENTE);
+        List<Multa> vencidas = multaRepository.findByInfractor_IdAndEstado(infractorId, EstadoMulta.VENCIDA);
+        double deudaPendiente = pendientes.stream().mapToDouble(Multa::getMonto).sum();
+        double deudaVencida = vencidas.stream().mapToDouble(m -> m.getMonto() * 1.15).sum();
+        return deudaPendiente + deudaVencida;
+    }
+
+    @Override
+    public void desasignarVehiculo(Long infractorId, Long vehiculoId){
+        Infractor infractor = infractorRepository.findById(infractorId).orElseThrow(() -> new InfractorNotFoundException(infractorId));
+        Vehiculo vehiculo = vehiculoRepository.findById(vehiculoId).orElseThrow(() -> new VehiculoNotFoundException(vehiculoId));
+        List<Multa> pendientes = multaRepository.findByVehiculo_IdAndEstado(vehiculoId, EstadoMulta.PENDIENTE);
+        if (!pendientes.isEmpty()) {
+            throw new RuntimeException("El vehículo tiene multas pendientes");
+        }
+        infractor.getVehiculos().remove(vehiculo);
+        boolean hayPendientes = multaRepository.findByVehiculo_IdAndEstado(vehiculoId, EstadoMulta.PENDIENTE).size() > 0;
+        if (!hayPendientes) {
+            vehiculo.setSuspendido(false);
+            vehiculoRepository.save(vehiculo);
+        }
+        infractorRepository.save(infractor);
+    }
+}
